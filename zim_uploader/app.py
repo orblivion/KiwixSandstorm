6a6717cfe6cc138126582febe08255ae7828638f
Daniel Krol <orblivion@gmail.com>
2016-05-12 13:49:04 -0700

    Uploading works when running the flask app locally. Now to try in Sandstorm.

--- zim_uploader/app.py
+++ zim_uploader/app.py
@@ -1,17 +1,34 @@
+import argparse
+import os
 import sys
 
 from flask import Flask, request, render_template
 
+parser = argparse.ArgumentParser(description='Serve Zim file uploader.')
+parser.add_argument('upload_path', type=str,
+                    help='Where the zim file should go')
+parser.add_argument('-d', dest='debug', action='store_true',
+                    help='Run this flask app in debug mode')
+
+args = parser.parse_args()
+
 app = Flask(__name__)
-if 'debug' in sys.argv:
-    app.debug = True
+app.debug = args.debug
+
 
 @app.route("/", methods=["GET", "POST"])
 def hello():
     if request.method == 'GET':
         return render_template('form.html')
-    else:
-        raise NotImplementedError
+    elif request.method == 'POST':
+        f = request.files['file_to_upload']
+        if not f.filename:
+            return render_template('form.html', unspecified=True)
+        elif f.mimetype != 'application/x-zim-notebook':
+            # I'm surprise this exists!
+            return render_template('form.html', bad_file_type=True)
+        f.save(os.path.join(args.upload_path, 'my.zim'))
+        return render_template('form.html', success=True)
 
 if __name__ == "__main__":
     app.run()
